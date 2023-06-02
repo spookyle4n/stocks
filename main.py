ef9f85a3c56490f6e94213b248702a012364fad9
spookyle4n <103913003+spookyle4n@users.noreply.github.com>
2023-06-02 14:25:12 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import requests
+
+api_key = 'YOUR_API_KEY'  # Replace with your actual Polygon.io API key
+
+def get_stock_info(symbol):
+    base_url = 'https://api.polygon.io/v2/reference/tickers'
+    params = {
+        'ticker': symbol,
+        'apiKey': api_key
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    if 'results' in data and len(data['results']) > 0:
+        stock = data['results'][0]
+        print('Company Name:', stock['name'])
+        print('Exchange:', stock['market'])
+        print('Country:', stock['country'])
+        print('Stock Type:', stock['type'])
+        print('Market Cap:', stock['marketCap'])
+        print('Description:', stock['description'])
+    else:
+        print('No information found for the given stock symbol.')
+
+def main():
+    symbol = input('Enter the stock symbol: ')
+    get_stock_info(symbol)
+
+if __name__ == '__main__':
+    main()
